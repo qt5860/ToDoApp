b0b6a7ff492b89cf04d32bc7add1734d45b5bd84
kimsumin <qt5860@naver.com>
2023-08-07 14:47:30 +0900

    String API

---
+++ src/main/java/com/codestates/ToDoApp/ToDoController.java
@@ -0,0 +1,14 @@
+package com.codestates.ToDoApp;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ToDoController {
+    @GetMapping("/")
+    public String hello(){
+        return "To-do Application !";
+    }
+
+}
